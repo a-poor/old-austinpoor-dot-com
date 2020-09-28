dfbe86687af5e2bc05cff6949e551e1061bcd614
a-poor <a-poor@users.noreply.github.com>
2020-09-27 22:25:58 -0400

    Adding tab content components. Filling in start of AboutMe. Changing favicon.

---
+++ src/About.js
@@ -0,0 +1,75 @@
+
+import React from 'react';
+
+import 'antd/dist/antd.css';
+import { Typography, Divider, List } from 'antd';
+
+import {
+    GithubOutlined,
+    LinkedinFilled,
+    MediumSquareFilled,
+    MailOutlined,
+} from '@ant-design/icons';
+
+// const { useState } = React;
+
+const { Title, Text, Link, Paragraph } = Typography;
+
+
+function About() {
+  const contactData = [
+      {icon:<GithubOutlined />,name:"GitHub",value:"a-poor",link:"https://github.com/a-poor"},
+      {icon:<LinkedinFilled />,name:"LinkedIn",value:"austinpoor",link:"https://linkedin.com/in/austinpoor"},
+      {icon:<MediumSquareFilled />,name:"Medium",value:"@apoor",link:"https://medium.com/@apoor"},
+      {icon:<MailOutlined />,name:"Email",value:"austinpoor@gmail.com",link:"mailto:austinpoor@gmail.com"}
+  ];
+  return (
+    <>
+        <Title>About Me</Title>
+        <Paragraph>
+            My name is Austin. I'm a Junior Data Scientist living in New York City.
+        </Paragraph>
+        <Paragraph>
+            In 2019, I graduated from Sarah Lawrence College with a bachelor's degree in computer science, and in 2020 I completed an intensive 12-week data science bootcamp at Metis. Between Sarah Lawrence and Metis, I learned 
+        </Paragraph>
+        <Paragraph>
+            Before Sarah Lawrence, I worked for 3 years at CHRLX, a commercial animation studio in NYC, as the Executive Creative Director's assistant. At CHRLX, I 
+        </Paragraph>
+
+        <Divider />
+
+        <Title level={3}>Contact Me</Title>
+
+        {/* <List
+            style={{ fontSize:20 }}
+            size="large"
+            header={""}
+            footer={""}
+            bordered
+            dataSource={contactData}
+            renderItem={item => (
+                <List.Item>
+                    {item.icon} <Text strong>{item.name}:</Text> <Link href={item.link} target="_blank">{item.value}</Link>
+                </List.Item>
+            )}
+        /> */}
+        <table style={{ fontSize:20, margin:0, padding:5 }}>
+            <tbody>
+                {contactData.map(item => (
+                    <tr>
+                        <td style={{paddingRight:5}}>{item.icon}</td>
+                        <td style={{padding:5}}>
+                            <Text strong>{item.name}:</Text>
+                        </td>
+                        <td style={{padding:5}}>
+                            <Link href={item.link} target="_blank">{item.value}</Link>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </>
+  );
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { Layout, Menu, Breadcrumb, Typography } from 'antd';
 
 import Home from "./Home.js"
 import Projects from "./Projects.js"
+import BlogPosts from "./BlogPosts.js"
+import About from "./About.js"
 
 const { useState } = React;
 
@@ -15,28 +17,34 @@ const { Title, Text, Link, Paragraph } = Typography;
 
 
 function App() {
-  const allTabs = {
-    "1": <Home />,
-    "2": <Projects />
+  const [ pageNum, setTab ] = useState("home");
+  const getPage = name => {
+    if (name === "home")
+      return <Home/>;
+    if (name === "projects") 
+      return <Projects/>;
+    if (name === "blog")
+      return <BlogPosts/>;
+    if (name === "about")
+      return <About/>;
   };
-  const [ pageTab, setTab ] = useState("");
-  // const pressPage => 
 
   return (
     <div className="App">
       <Header style={{ position: 'inline', zIndex: 1, width: '100%' }}>
         <div className="logo"></div>
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1">Home</Menu.Item>
-          <Menu.Item key="2">Projects</Menu.Item>
-          <Menu.Item key="3">Blog</Menu.Item>
-          <Menu.Item key="4">About</Menu.Item>
+          <Menu.Item key="1" onClick={() => setTab("home")}>Home</Menu.Item>
+          <Menu.Item key="2" onClick={() => setTab("projects")}>Projects</Menu.Item>
+          <Menu.Item key="3" onClick={() => setTab("blog")}>Blog Posts</Menu.Item>
+          <Menu.Item key="4" onClick={() => setTab("about")}>About Me</Menu.Item>
         </Menu>
       </Header>
       <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
         {/* <Title>AustinPoor.com</Title> */}
         <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
-          <Home />
+          {/* <Home /> */}
+          { getPage(pageNum) }
         </div>
       </Content>
       <Footer style={{ textAlign: "center" }}>
