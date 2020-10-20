e89c447ea8838d02d9ed0feaf75d931dc546bdff
a-poor <45295232+a-poor@users.noreply.github.com>
2020-10-20 18:13:58 -0400

    Fixing vulnerability and adding build

---
+++ build/precache-manifest.90e09845c010b19dc91d465f49bb1217.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3fb08fa468790dfcc507c9e588e5f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "180a33327c649f03502d",
+    "url": "/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "3f3b3bdb68b6b684c4d7",
+    "url": "/static/css/main.45008c15.chunk.css"
+  },
+  {
+    "revision": "180a33327c649f03502d",
+    "url": "/static/js/2.9fa0def7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9fa0def7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f3b3bdb68b6b684c4d7",
+    "url": "/static/js/main.4501925d.chunk.js"
+  },
+  {
+    "revision": "57e493684bb470133b59",
+    "url": "/static/js/runtime-main.d15bdf63.js"
+  },
+  {
+    "revision": "c8b45ebf3bb6ad77b07c3b2db79306d4",
+    "url": "/static/media/logo.c8b45ebf.svg"
+  }
+]);
