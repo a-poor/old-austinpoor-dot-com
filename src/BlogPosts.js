b11c534e3e55ed91f2884712afa6042294d5678d
a-poor <45295232+a-poor@users.noreply.github.com>
2020-10-20 22:04:27 -0400

    Changing verbiage on blog posts page

--- src/BlogPosts.js
+++ src/BlogPosts.js
@@ -28,8 +28,7 @@ function BlogPosts() {
         <Divider />
         {/* <LoadingBlogPosts /> */}
         <Paragraph>
-            {/* You can check out my blog posts <Link href="https://medium.com/@apoor">here</Link>! */}
-            Check out my blog posts on <Link href="https://medium.com/@apoor">Medium</Link>!
+            This part of the site is still under construction. For now, check out my blog posts on <Link href="https://medium.com/@apoor">Medium</Link>!
         </Paragraph>
     </>
   );
